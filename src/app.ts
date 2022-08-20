176b85b66967c43fd51e7d09142a525c55f4925f
julien-guillan <guillan.julien@live.com>
2022-08-20 15:11:37 +0200

    List of restaurants ok

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import fs from 'fs';
 import https from 'https';
 import jsdom from 'jsdom';
-import zlib from 'zlib';
 
 const app = express();
 const { JSDOM } = jsdom;
 const PORT = 3000;
+const filename = 'macdo-restaurants-paris.json';
 
-const options = {
+const options: any = {
     method: 'GET',
     hostname: 'www.mcdonalds.fr',
     path: '/liste-restaurants-mcdonalds-france',
@@ -30,16 +31,33 @@ const options = {
     maxRedirects: 20,
   };
 
-function fetchRestaurants(): void {
-    let urls = [];
+function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+// TODO array of restaurants in file
+async function fetchRestaurants(): Promise<void> {
+    const path = '/liste-restaurants-mcdonalds-france';
+    const urls = [];
+    const restaurants = [];
     const chunks = [];
+    options.path = path;
+    if (fs.existsSync(filename)) {
+        fs.copyFileSync(filename, `${filename}.copy`);
+    }
+    console.log('Erasing file');
+    fs.writeFileSync(filename, '');
+    console.log('Fetching all restaurants...');
     const req = https.request(options, (res) => {
         res.on('data', (chunk: Buffer) => {
-            console.log(res.statusCode);
             chunks.push(chunk);
         });
 
         res.on('end', () => {
+            console.log(`Status code: ${res.statusCode}`);
+            if (res.statusCode !== 200) { throw new Error(`Error while connecting to mcdonald's website`); }
+            fs.rmSync(`${filename}.copy`);
+            console.log('Fetching restaurant data...');
             const body = Buffer.concat(chunks).toString();
             const dom = new JSDOM(body);
             const doc = dom.window.document as HTMLHtmlElement;
@@ -47,26 +65,84 @@ function fetchRestaurants(): void {
             for (const td of tds) {
                 const tdHtml = td.innerHTML;
                 const url = tdHtml.split('"')[1];
-                if(url.indexOf('paris') !== -1) {
+                if (url.indexOf('paris') !== -1) {
                     urls.push(url);
                 }
             }
-            console.log(urls);
+            // tslint:disable-next-line: forin
+            for (const index in urls) {
+                sleep(100).then(() => {
+                    const restoChunks = [];
+                    const splitUrl = urls[index].split('/');
+                    const restaurantId = splitUrl[splitUrl.length - 1];
+                    console.log(`${index} => Fetching data restaurant id: ${restaurantId}`);
+                    const restReq = https.request(
+                            `https://ws.mcdonalds.fr/api/restaurant/${restaurantId}
+                            ?responseGroups=RG.RESTAURANT.ADDRESSES
+                            &responseGroups=RG.RESTAURANT.FACILITIES
+                            &responseGroups=RG.RESTAURANT.NEWS
+                            &responseGroups=RG.RESTAURANT.PICTURES
+                            &responseGroups=RG.RESTAURANT.METADATAS`,
+                        (response) => {
+                            response.on('data', (chunk: Buffer) => {
+                            restoChunks.push(chunk);
+                        });
+
+                            response.on('end', () => {
+                                if (response.statusCode !== 200) { throw new Error(`Error while connecting to mcdonald's website`); }
+                                const restaurant = JSON.parse(Buffer.concat(restoChunks).toString());
+                                restaurants.push(restaurant);
+                                console.log(`${index} => Writing data to file: ${filename}`);
+                                // console.log(restaurant);
+                                fs.appendFileSync(filename, `${JSON.stringify(restaurant, null, 4)},`);
+                                console.log(`${index} => Request ended, restaurant ${restaurantId}: ${res.statusCode}`);
+                        });
+
+                            response.on('error', (err) => {
+                            throw err;
+                        });
+                    });
+                    restReq.end();
+                });
+            }
         });
 
         res.on('error', (err) => {
-            console.error(err);
+            throw(err);
         });
     });
 
     req.end();
 }
 
-app.get('/list/:city', (req, res) => {
-    res.send(req.params.city);
+app.get('/list', (req, res) => {
+    const fileContent = JSON.parse(fs.readFileSync(filename).toString());
+    const names = [];
+    // tslint:disable-next-line: forin
+    for (const index in fileContent) {
+        const restaurant = fileContent[index];
+        names.push(restaurant.name);
+    }
+    res.send(names);
 });
 
-app.listen(PORT, () => {
-    fetchRestaurants();
-    console.log(`service listening on port ${PORT}`);
-});
+// fetchRestaurants();
+
+async function main(): Promise<number> {
+    if (process.argv.length > 2) {
+        const params = process.argv.filter((arg, index) => index > 1);
+        if (params.includes('update')) {
+            console.log('=======UPDATE RESTAURANTS LIST=======');
+            await fetchRestaurants();
+        }
+
+        if (params.includes('start')) {
+            app.listen(PORT, () => {
+                console.log(`Mcdo comm api listening on port ${PORT}`);
+            });
+        }
+    }
+    return 0;
+}
+
+main();
